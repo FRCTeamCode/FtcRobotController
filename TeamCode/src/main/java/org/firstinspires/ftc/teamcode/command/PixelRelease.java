04bb6843781c7d534ee869c44423a76d73fa00d9
QKai0314 <kai0314@hotmail.com>
2024-08-12 13:59:40 +0800

    Update PixelRelease.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/PixelRelease.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/PixelRelease.java
@@ -11,13 +11,13 @@ import org.firstinspires.ftc.teamcode.subsystem.Intake;
 public class PixelRelease extends SequentialCommandGroup {
     public PixelRelease(Intake intake) {
         addCommands(
-                new IntakeControl(intake, 2.0),
+                new IntakeControl(intake, 2.0),//2.0---0.25
                 new WaitCommand(150),
-                new IntakeControl(intake, 4.0),
+                new IntakeControl(intake, 4.0),//4.0---0.28
                 new WaitCommand(150),
-                new IntakeControl(intake, 5.0),
+                new IntakeControl(intake, 5.0),//5.0---0.3
                 new WaitCommand(150),
-                new IntakeControl(intake, 6.0)
+                new IntakeControl(intake, 6.0)//6.0---0.32
         );
         addRequirements(intake);
     }
